3a74533393ed684d8e621a028902ca8ce458e1df
Matheus Tontini <tontini92@gmail.com>
2020-02-10 21:32:54 +0000

    pagination and avatar added

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,12 +1,33 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
-  async index(res, req) {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
-      where: { user_id: req.userId, cancelled_at: null }
+      where: { user_id: req.userId, cancelled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url']
+            }
+          ]
+        }
+      ]
     });
 
     return res.json(appointments);
