8df1150d886a7c004127b27c5a84afb17be3786f
Matheus Tontini <tontini92@gmail.com>
2020-02-16 17:01:24 +0000

    environment variables

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '4644DB13F5494120FE57E969F9A1DF62',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d'
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '47c3f0f539bd68',
-    pass: '806de055706023'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   default: {
     from: 'GoBarber Team <noreply@gobarber.com>'
